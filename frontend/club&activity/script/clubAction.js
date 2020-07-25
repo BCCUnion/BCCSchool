21e6ce483954009cf0fdd4ba176a82a4bd717756
Labo_9527 <2543761065@qq.com>
2020-07-25 10:01:36 +0800

    Update clubAction.js

--- frontend/club&activity/script/clubAction.js
+++ frontend/club&activity/script/clubAction.js
@@ -1,12 +1,14 @@
 window.onload = function(){
-    // console.log('123')
+
+    // 根据屏幕宽度定位所有信息框
     windoWidth = parseInt(window.screen.width)
     blocks = document.getElementsByClassName('clubInfo');
     for(var i =0;i<blocks.length;i++){
         blocks[i].style.left = (windoWidth-450)/2*(i%3) + 50 + 'px';
         blocks[i].style.top = parseInt(i/3) * 400 + 80 +'px';
     }
-
+    
+    // 为所有按钮绑定点击函数
     buttons = document.getElementsByClassName('clubbutton');
     for(var i=0;i<buttons.length;i++){
         buttons[i].onclick = function(){
