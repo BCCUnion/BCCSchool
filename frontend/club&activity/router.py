99d22245c7099aeccdc377c0963b2682e31cf950
Labo_9527 <2543761065@qq.com>
2020-07-25 09:45:57 +0800

    Update router.py

--- frontend/club&activity/router.py
+++ frontend/club&activity/router.py
@@ -4,16 +4,18 @@ import csv
 import pandas as pd
 app = Flask(__name__)
 
+# 对俱乐部报名的路由
 @app.route('/', methods=["POST"])
 def club():
+    # 先打印请求包的信息
     print(request.headers)
-    # print(request.stream.read())
     print(request.form)
     name = request.form.get('name')
     number = request.form.get('number')
     clubname = request.form.get('clubname')
     ps = request.form.get('ps')
 
+    # 写入csv数据库
     with open('./csv/club.csv','a+') as f:
         csv_write = csv.writer(f)
         data_row = [name, number, clubname, ps]
@@ -21,16 +23,18 @@ def club():
 
     return '<h1>Hello World</h1>'
 
+# 对活动报名的路由
 @app.route('/activ', methods=["POST"])
 def activ():
+    # 先打印请求包的信息
     print(request.headers)
-    # print(request.stream.read())
     print(request.form)
     name = request.form.get('name')
     number = request.form.get('number')
     activname = request.form.get('activname')
     ps = request.form.get('ps')
 
+    # 写入csv数据库
     with open('./csv/activ.csv','a+') as f:
         csv_write = csv.writer(f)
         data_row = [name, number, activname, ps]
