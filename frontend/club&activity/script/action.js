fdfb4cbdefb985e9bd14c44a939347bfb70f7b93
Labo_9527 <2543761065@qq.com>
2020-07-25 10:00:42 +0800

    Update action.js

--- frontend/club&activity/script/action.js
+++ frontend/club&activity/script/action.js
@@ -3,7 +3,7 @@ var imgs = ['url(./imgs/img1.jpg)', 'url(./imgs/img2.jpg)']
 
 window.onload=function(){
 
-    # 实现搜索功能，按照关键词进行搜索
+    // 实现搜索功能，按照关键词进行搜索
     
     function t(){
         textvalue = document.getElementsByName('clubSearch')[0].value;
@@ -49,56 +49,56 @@ window.onload=function(){
         window.open("activPage.html")
     }
     
-    # 根据屏幕宽度定位搜索框
+    // 根据屏幕宽度定位搜索框
     document.getElementById('forInput').style.width='460px';
     document.getElementById('forInput').style.left=(parseInt(window.screen.width)-460)/2+'px';
 
-    # moving用来判断图片是否在移动，一开始为false
+    // moving用来判断图片是否在移动，一开始为false
     moving=false;
     myLeft = -parseInt(window.screen.width);
     
-    # 由于图片列表左边额外有一个图片，所以一开始图片的left是负一倍屏幕宽度
+    // 由于图片列表左边额外有一个图片，所以一开始图片的left是负一倍屏幕宽度
     document.getElementById("imgList").style.left=myLeft+'px';
 
-    # 把图片列表中所有的图片都按顺序排好
+    // 把图片列表中所有的图片都按顺序排好
     var imgs = document.getElementsByClassName("imgs");
     document.getElementById("imgList").style.width=imgs.length*window.screen.width+'px';
     for(var i=0;i<imgs.length;i++){
         imgs[i].style.width=window.screen.width+'px';
     }
     
-    # 图片列表移动函数，distance为移动的距离，可正可负
+    // 图片列表移动函数，distance为移动的距离，可正可负
     function move(distance){
 
-        # moving置为true，并计算出移动终点
+        // moving置为true，并计算出移动终点
         moving=true;
         list = document.getElementById("imgList");
         var endPoint = parseInt(list.style.left) + distance;
 
-        # 设定一个函数，用来图片切换
+        // 设定一个函数，用来图片切换
         function switchImg(){
             
-            # 打印图片位置和单次移动距离（总距离的五十分之一）
+            // 打印图片位置和单次移动距离（总距离的五十分之一）
             console.log(parseInt(list.style.left));
             console.log(distance/50);
             
-            # 如果没有移动到终点，则继续移动
+            // 如果没有移动到终点，则继续移动
             if((parseInt(list.style.left)<endPoint&&distance>0)||(parseInt(list.style.left) >endPoint&&distance<0)){
                 
-                # 设定单次移动后的位置
+                // 设定单次移动后的位置
                 myLeft=parseInt(list.style.left)+distance/10;
                 myLeft = myLeft + 'px';
                 list.style.left = myLeft;
                 
-                # 50ms之后进行下一次单次移动
+                // 50ms之后进行下一次单次移动
                 setTimeout(switchImg, 50);
             }
             else{
                 
-                # 如果移动到终点即moving置false
+                // 如果移动到终点即moving置false
                 moving=false;
                 
-                # 如果图片列表到最左端图片，则将其瞬间移动到倒数第二张图片的位置，以实现轮转的效果
+                // 如果图片列表到最左端图片，则将其瞬间移动到倒数第二张图片的位置，以实现轮转的效果
                 if(parseInt(list.style.left)>=0){
                     myLeft=-window.screen.width*(imgs.length-2);
                     myLeft = myLeft + 'px';
@@ -107,7 +107,7 @@ window.onload=function(){
                     list.style.left = myLeft;
                 }
                 
-                # 如果列表到最右端图片，则将其瞬间移动到第二张图片的位置，以实现轮转的效果
+                // 如果列表到最右端图片，则将其瞬间移动到第二张图片的位置，以实现轮转的效果
                 bound = -parseInt(window.screen.width)*(imgs.length-1)
                 console.log('bound'+bound)
                 if(parseInt(list.style.left)<=bound){
@@ -116,7 +116,7 @@ window.onload=function(){
                     list.style.left = myLeft;
                 }
 
-                # 将所有小圆点的状态更新，只有当前显示图片的小圆点状态为light
+                // 将所有小圆点的状态更新，只有当前显示图片的小圆点状态为light
                 dots = document.getElementsByClassName('dot');
                 myWidth = parseInt(window.screen.width)
                 if(parseInt(list.style.left)==-1*myWidth){
@@ -169,26 +169,26 @@ window.onload=function(){
             }
         }
 
-        # 调用切换图片函数
+        // 调用切换图片函数
         switchImg();
 
     }
 
-    # 为左箭头点击绑定函数
+    // 为左箭头点击绑定函数
     document.getElementById('left').onclick=function(){
         if(!moving){
             move(parseInt(window.screen.width));
         }
     }
 
-    # 为右箭头点击绑定函数
+    // 为右箭头点击绑定函数
     document.getElementById('right').onclick=function(){
         if(!moving){
             move(-parseInt(window.screen.width));
         }
     }
 
-    # 所有圆点要根据屏幕宽度定位
+    // 所有圆点要根据屏幕宽度定位
     targets = new Array();
     for(var i=0;i<imgs.length-2;i++){
         targets[i]=-parseInt(window.screen.width)*(i+1); 
@@ -253,7 +253,7 @@ window.onload=function(){
         }
     }
 
-    # 2秒自动点击一次右箭头，实现自动播放的效果
+    // 2秒自动点击一次右箭头，实现自动播放的效果
     setInterval(function(){
         document.getElementById('right').onclick();
     },2000);
