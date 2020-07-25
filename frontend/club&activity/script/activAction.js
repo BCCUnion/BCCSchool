ce741898f1e1270da5fa5c561d4c8def5c8e2986
Labo_9527 <2543761065@qq.com>
2020-07-25 10:02:09 +0800

    Update activAction.js

--- frontend/club&activity/script/activAction.js
+++ frontend/club&activity/script/activAction.js
@@ -1,5 +1,6 @@
 window.onload = function(){
-    // console.log('123')
+    
+    // 根据屏幕宽度定位所有信息框
     windoWidth = parseInt(window.screen.width)
     blocks = document.getElementsByClassName('activInfo');
     for(var i =0;i<blocks.length;i++){
@@ -7,6 +8,7 @@ window.onload = function(){
         blocks[i].style.top = parseInt(i/3) * 400 + 80 +'px';
     }
 
+    // 为所有按钮绑定点击函数
     buttons = document.getElementsByClassName('activbutton');
     for(var i=0;i<buttons.length;i++){
         buttons[i].onclick = function(){
